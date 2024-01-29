abb98916f03dd5be236676de2327eef1e5743817
Hiura Mihate <tillyourteargoes@proton.me>
2024-01-29 16:02:35 +0700

    fire: set TW_SKIP_ADDITIONAL_FSTAB
    in hopes that it boot faster...
    
    Signed-off-by: Hiura Mihate <tillyourteargoes@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -104,6 +104,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 
 # Fstab
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
+TW_SKIP_ADDITIONAL_FSTAB := true
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
