9160639de768d5b0042b13a944ce9469c3426fb9
YudhoPatrianto <kydh01123@gmail.com>
2025-06-15 18:00:23 +0700

    [TEMP] BoardConfig: Don't build BUILD BROKEN
    if use build broken build size will reduce
    
    Signed-off-by: YudhoPatrianto <kydh01123@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,8 +9,6 @@ DEVICE_PATH := device/xiaomi/fire
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
-BUILD_BROKEN_DUP_RULES := true
-BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 # System As Root
 BOARD_SUPPRESS_SECURE_ERASE := true
