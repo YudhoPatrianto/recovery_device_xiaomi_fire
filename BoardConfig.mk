482e4634b27a2006ca9a1b35332efccd5d0a29e0
puspita nilam <puspitanilam931@gmail.com>
2025-07-06 15:49:57 +0700

    fire: BoardConfig: make vbmeta images hastree disabled
    Signed-off-by: puspita nilam <puspitanilam931@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -108,6 +108,13 @@ BOARD_AVB_ALGORITHM := SHA256_RSA2048
 BOARD_AVB_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
 
+# boot 
+BOARD_AVB_BOOT_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
+BOARD_AVB_BOOT_ALGORITHM := SHA256_RSA2048
+BOARD_AVB_BOOT_ROLLBACK_INDEX := 1
+BOARD_AVB_BOOT_ROLLBACK_INDEX_LOCATION := 1
+
+
 # vbmeta_system
 BOARD_AVB_VBMETA_SYSTEM := system system_ext product
 BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
