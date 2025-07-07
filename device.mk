d2e0b02e307d6b62fd87277ccf7210e918945d7d
YudhoPatrianto <kydh01123@gmail.com>
2025-07-07 18:58:36 +0000

    fire: device.mk: reworking Update Engine & Update Verifier
    Signed-off-by: YudhoPatrianto <kydh01123@gmail.com>

--- device.mk
+++ device.mk
@@ -17,20 +17,30 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 # Update Engine & Update Verifier 
 PRODUCT_PACKAGES += \
     update_engine \
-    update_verifier \
-    update_engine_sideload
+    update_engine_sideload \
+    update_verifier
 
-# A/B
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
+
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
-# A/B
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=erofs \
+    POSTINSTALL_OPTIONAL_vendor=true
+
 PRODUCT_PACKAGES += \
-    otapreopt_script \
-    cppreopts.sh
+    update_engine \
+    update_engine_sideload \
+    update_verifier \
+    checkpoint_gc \
+    otapreopt_script
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
