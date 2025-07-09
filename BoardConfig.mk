33b96fc0d196ca86208ce0d970502c4fb6eae36a
YudhoPatrianto <kydh01123@gmail.com>
2025-07-09 13:27:21 +0000

    Revert "fire: BoardConfig: handle `data/media`"
    This reverts commit 5cbc41d540a903d7c8d32a0528904b4f8e4b6c7d.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -162,9 +162,6 @@ TW_HAS_MTP := true
 # USB OTG
 TW_USB_STORAGE := true
 
-# Support /data/media
-RECOVERY_SDCARD_ON_DATA := true
-
 # LOGCAT
 TARGET_USES_LOGD := true    
 TWRP_INCLUDE_LOGCAT := true
