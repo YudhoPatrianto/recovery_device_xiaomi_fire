61bd9ed199c73fc1c6a162532282d20a252e6174
YudhoPatrianto <kydh01123@gmail.com>
2025-07-18 09:15:23 +0700

    fire: device.mk: remove duplicate checkpoint_gc and otapreopt_script
    Signed-off-by: YudhoPatrianto <kydh01123@gmail.com>

--- device.mk
+++ device.mk
@@ -39,12 +39,6 @@ PRODUCT_PACKAGES += \
     checkpoint_gc \
     otapreopt_script
 
-PRODUCT_PACKAGES += \
-    update_engine \
-    update_engine_sideload \
-    update_verifier
-
-
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-impl \
